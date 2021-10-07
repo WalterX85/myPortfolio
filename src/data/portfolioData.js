9bcc2aaa77d43e4b2a5ac82306e6b9c442183cbe
WalterX85 <cedric.guyot2@gmail.com>
2021-10-07 20:19:42 +0200

    last update

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,56 +1,85 @@
+//'fab fa-php'
+
 export const portfolioData = [
   {
     id: 1,
-    name: 'ipsum.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
+    name: 'Blog articles',
+    languages: ['react'],
+    languagesIcons: ['fab fa-react'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project1.JPG'
+    info: "A partir de l'API jsonplaceholder, je génère des articles dans mon blog. Nous avons aussi la possibilité de publier nous meme notre post.  #React, #Redux, #CSS",
+    picture: './media/Blog.png'
   },
   {
     id: 2,
-    name: 'lorem.fr',
-    languages: ['javascript','react'],
-    languagesIcons: ['fab fa-js','fab fa-react'],
+    name: 'Floppy Bird',
+    languages: ['javascript'],
+    languagesIcons: ['fab fa-js'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project2.JPG'
+    info: "Copie du célèbre jeu déchu en 2014 accusé de plagiat graphique avec l'univers Mario. Le gameplay repose sur l'agilité du joueur, qui doit faire avancer un oiseau dans un environnement à défilement horizontal en tapotant sur l'écran tactile, tout en évitant des tuyaux présents en haut et en bas de l'écran. Les règles de jeu sont très simples : lorsque l'oiseau touche un tuyau ou heurte le sol, la partie est terminée. Le joueur reçoit un point pour chaque tuyau que l'oiseau évite. #Javascript",
+    picture: './media/fb.jpg'
   },
   {
     id: 3,
-    name: 'dolorset.fr',
-    languages: ['javascript','php','css'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-css3-alt'],
+    name: 'Quizz',
+    languages: ['javascript','css'],
+    languagesIcons: ['fab fa-js','fab fa-css3-alt'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project3.JPG'
+    info: "Le quizz est un exercice interessant lorsque l'on débute en programmation. En javascript pur, il y a une grosse manipulation du DOM qu'il est toujours bon de revoir lorsque l'on passe un peu trop de temps sur React. :p",
+    picture: './media/quizz.png'
   },
   {
     id: 4,
-    name: 'laboriosam.fr',
-    languages: ['javascript','php','react'],
-    languagesIcons: ['fab fa-js','fab fa-php','fab fa-react'],
+    name: 'Le juste prix',
+    languages: ['javascript'],
+    languagesIcons: ['fab fa-js'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project4.JPG'
+    info: "Mon tout premier jeu. J'ai conscience que ça ne casse pas 3 pattes à un canard dans un portfolio mais le coeur a ses raisons que la raison ignore.",
+    picture: './media/game.png'
   },
   {
     id: 5,
-    name: 'repudiante.com',
-    languages: ['javascript','css'],
-    languagesIcons: ['fab fa-js', 'fab fa-css3-alt'],
+    name: 'Authentification Firebase',
+    languages: ['javascript'],
+    languagesIcons: ['fab fa-js'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project5.JPG'
+    info: "Firebase est un outil proposé par Google pour gérer des bases de données et pour certaines fonctionnalités comme l'authentification. Dans ce projet nous pourrons nous inscrire, nous connecter et nous déconnecter avec une gestion de contenu public et privé.",
+    picture: './media/firebase.png'
   },
   {
     id: 6,
-    name: 'test.io',
-    languages: ['javascript','react', 'css'],
-    languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
+    name: 'DarkMode',
+    languages: ['react'],
+    languagesIcons: ['fab fa-react'],
     source: 'http://www.github.com',
-    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-    picture: './media/project6.JPG'
+    info: "Utilisation de l'api de context pour créer une fonctionnalité dark mode.",
+    picture: './media/dark.png'
+  },
+  {
+  id: 7,
+  name: 'Homepage',
+  languages: ['css'],
+  languagesIcons: ['fab fa-css3-alt'],
+  source: 'http://www.github.com',
+  info: "Réalisation d'une page d'accueil responsive façon streaming",
+  picture: './media/homepage.png'
+  },
+  {
+  id: 8,
+  name: 'Homepage',
+  languages: ['css'],
+  languagesIcons: ['fab fa-css3-alt'],
+  source: 'http://www.github.com',
+  info: "Réalisation d'une page d'accueil mode avec background vidéo.",
+  picture: './media/ny.png'
+  },
+  {
+  id: 9,
+  name: 'test.io',
+  languages: ['javascript','react', 'css'],
+  languagesIcons: ['fab fa-js','fab fa-react','fab fa-css3-alt'],
+  source: 'http://www.github.com',
+  info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+  picture: './media/project6.JPG'
   },
 ]
